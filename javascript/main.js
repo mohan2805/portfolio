3ee61fab4533127e8bfad219000a3470872c9053
mohan2805 <149153937+mohan2805@users.noreply.github.com>
2025-04-23 16:02:34 +0530

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+var navlinks = document.getElementById("navlinks");
+var x_btn = document.getElementById("x_btn");
+function showmenu() {
+    navlinks.style.right = "0";
+    x_btn.style.display = "block";
+}
+function hidemenu() {
+    navlinks.style.right = "-300px";
+    // x_btn.style.display = "none";
+
+}
